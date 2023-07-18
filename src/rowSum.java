e3239b18edde655f7d8f5e05b0bf6d6e9a9a4436
kishoreguptha <kishoreguptha2002@gmail.com>
2023-07-18 10:38:13 +0530

    row sum

---
+++ src/rowSum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class rowSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowSize = sc.nextInt();
+        int columnSize = sc.nextInt();
+        int array[][] = new int[rowSize][columnSize];
+        for(int i=0;i<rowSize;i++)
+        {
+            for(int j=0;j<columnSize;j++)
+            {
+                array[i][j]=sc.nextInt();
+            }
+        }
+        rowSum(array,rowSize,columnSize);
+    }
+    public static void rowSum(int[][] array,int row,int column)
+    {
+        int res[] = new int[row];
+        for(int i=0;i<row;i++)
+        {
+            int sum=0;
+            for(int j=0;j<column;j++)
+            {
+                sum+=array[j][i];
+            }
+            res[i] = sum;
+        }
+        System.out.println(Arrays.toString(res));
+    }
+}
